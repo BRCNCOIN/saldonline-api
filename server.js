4a27936298a664b608bc24ac2c02ecacd50cb46a
Robson Oliveira <200010598+BRCNCOIN@users.noreply.github.com>
2025-05-01 21:22:30 -0300

    Atualização

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+server.use(router);
+
+server.listen(process.env.PORT || 10000, () => {
+  console.log("JSON Server rodando com CORS ativo");
+});
